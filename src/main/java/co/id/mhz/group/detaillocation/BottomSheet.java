53e01f5ccec5c957556fd88e0e1bdabdfa66367f
Rezha <muhrez2012@gmail.com>
2023-05-27 10:51:05 +0700

    fixing

--- src/main/java/co/id/mhz/group/detaillocation/BottomSheet.java
+++ src/main/java/co/id/mhz/group/detaillocation/BottomSheet.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class BottomSheet {
+
+    public BottomSheet() {
+    }
+
     public static void s(Context c, String message){
 
         Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
