442f1151c43d14ecd63b3f5449dbf0446050e9bb
Rezha <muhrez2012@gmail.com>
2023-05-27 10:09:42 +0700

    initial commit

---
+++ src/main/java/co/id/mhz/group/detaillocation/BottomSheet.java
@@ -0,0 +1,12 @@
+package co.id.mhz.group.detaillocation;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class BottomSheet {
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+
+    }
+}
---
+++ src/main/java/co/id/mhz/group/detaillocation/DetailLocation.java
@@ -0,0 +1,39 @@
+package co.id.mhz.group.detaillocation;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+
+import java.text.DecimalFormat;
+import java.util.List;
+
+public class DetailLocation {
+
+    public String detailLocation(Context context, String lat, String longi){
+//        String lat = "-6.278065";
+//        String longi = "106.854912";
+        Geocoder geocoder = new Geocoder(context);
+        List<Address> addressList;
+        String result = "";
+        try {
+            addressList = geocoder.getFromLocation(
+                    Double.parseDouble(lat),
+                    Double.parseDouble(longi), 1);
+            if (addressList != null && addressList.size() > 0) {
+                String locality = addressList.get(0).getAddressLine(0);
+                String country = addressList.get(0).getCountryName();
+                if (locality == null && country == null){
+                    return "Location not found";
+                }
+                else {
+                    result = locality;
+                    return result;
+                }
+            }
+        }catch (Exception e){
+            result = e.getMessage().toString();
+        }
+
+        return result;
+    }
+}
